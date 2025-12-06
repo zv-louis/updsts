8221ce8e5d50a5ebd7cc66737e3f882eacb2f480
kanok-one <38430015+zv-louis@users.noreply.github.com>
2025-12-06 10:49:42 +0900

    added workaround for antigravity

--- src/updsts/__init__.py
+++ src/updsts/__init__.py
@@ -1,1 +1,11 @@
 ﻿# encoding: utf-8-sig
+
+import sys
+import io
+
+# workaround for antigravity issues on Windows
+if sys.platform.startswith("win"):
+    sys.stdout = io.TextIOWrapper(sys.stdout.buffer,
+                                  encoding="utf-8",
+                                  newline="\n",
+                                  write_through=True)
